0733e2ded230562bc6a59494cc468995acd958ba
AggieWB <usmaggie23@gmail.com>
2020-08-13 20:49:26 -0700

    Require and initialize autoIncrement module
    Pass autoIncrement.plugin to the plugin() function of the userSchema with a set number to start and increment

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
 require('dotenv').config();
 mongoose.connect(process.env.MLAB_URI, {
   useNewUrlParser: true,
@@ -6,10 +7,18 @@ mongoose.connect(process.env.MLAB_URI, {
 });
 
 mongoose.connection.on('open', () => console.log('Mongoose connected'));
+autoIncrement.initialize(connection);
 mongoose.connection.on('error', err => console.log(`Mongoose could not connect: ${err}`));
 
 const userSchema = mongoose.Schema({
+  user: String
+});
 
+userSchema.plugin(autoIncrement.plugin, {
+  model: 'User',
+  field: '_id',
+  startAt: 1000,
+  incrementBy: 1
 });
 
 module.exports = mongoose.model('User', userSchema);
