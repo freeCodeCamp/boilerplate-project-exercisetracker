dda6d30aef2b49e417f73b0fba34e248da8c8d51
AggieWB <usmaggie23@gmail.com>
2020-08-20 09:51:46 -0700

    Install moment-timezone and require package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const moment = require('moment-timezone');
 const { User, Exercise } = require('./models/User.js');
 
 const cors = require('cors');
