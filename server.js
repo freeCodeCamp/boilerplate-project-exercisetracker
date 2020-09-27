95f8fcb79fa001686b90d26641767325c8fb3885
choilm <choilmto@gmail.com>
2020-09-27 09:25:50 -0400

    Integration test works on endpoint for root

--- server.js
+++ server.js
@@ -43,3 +43,8 @@ app.use((err, req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+module.exports = {
+  listener,
+  mongoose,
+};
---
+++ server.test.js
@@ -0,0 +1,22 @@
+const supertest = require("supertest");
+process.env.DB_URI = process.env.MONGO_URL;
+process.env.PORT = 0;
+const { mongoose, listener } = require("./server");
+
+let request;
+
+beforeAll(() => {
+  request = supertest(listener);
+});
+
+afterAll(() => {
+  return Promise.all([mongoose.disconnect(), listener.close()]);
+});
+
+describe("GET", () => {
+  test("/", async () => {
+    const response = await request.get("/");
+
+    expect(response.status).toBe(200);
+  });
+});
