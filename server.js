783c9de4aa5cd13cff56b7588a2b19ca107f9f24
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-10 16:09:57 -0600

    feat: remove error handling

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 require('dotenv').config()
 const cors = require('cors')
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useMongoClient: true } )
-
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -19,29 +13,8 @@ app.get('/', (req, res) => {
 });
 
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
