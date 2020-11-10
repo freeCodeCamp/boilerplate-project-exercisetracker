f9f7c040dd89f5255494f917e4e45738bde33b6c
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-10 13:19:05 -0600

    fix: remove assets + add sample.env, dotenv, gitignore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+require('dotenv').config()
 const cors = require('cors')
 
 const mongoose = require('mongoose')
