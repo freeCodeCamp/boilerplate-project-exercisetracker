729f781a3fb486f45c70d45475105117531765b4
al sew <ajsewell13@gmail.com>
2021-02-12 23:00:16 +0000

    finished POST route for exercise/add

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -2,10 +2,10 @@ const db = require('../db')
 const Schema = db.Schema;
 
 const exerciseSchema = new Schema({
-  userId: String,
-  duration: String,
-  description: String,
-  date: Date
+  userId: { type: String, required: true},
+  duration: { type: Number, required: true},
+  description: { type: String, required: true},
+  date: { type: Date, default: Date.now() }
 });
 const exerciseModel = db.model('exercise', exerciseSchema);
 
--- routes/exerciseRouter.js
+++ routes/exerciseRouter.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: false}));
 const exerciseModel = require('../models/exerciseModel.js');
+const userModel = require('../models/userModel.js');
 
 router.post('/add', (req, res) => {
-  res.json({ message: "add exercise" })
-})
+  let body = req.body;
+  userModel.find({_id: body.userid}, (err, result) => {
+    if (result == 0) res.json({error: 'no user matching given ID'})
+    else {
+      let user = result[0]
+      let newExercise = new exerciseModel({
+        userId: body.userid,
+        description: body.description,
+        duration: body.duration,
+        date: body.date == 0 ? Date.now() : body.date
+      });
+      newExercise.save((err, data) => {
+        if (err) return res.json({err})
+        else res.json({ message: "added exercise", data });
+        console.log('adding ' + data)
+      })
+    }
+  });
+});
 
 router.get('/log', (req, res) => {
-  res.json({ message: "getting exercise log"})
-})
+  userModel.find({_id: req.query.userid}, (err, log) => {
+    if (log == 0) res.json({error: 'no user matching given ID'});
+    else {
+      res.json({ log });
+    }
+  });
+});
 
 module.exports = router
