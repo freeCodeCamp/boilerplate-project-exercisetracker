b45c7686643953f4c3480573a97b375284dde521
ansxy <53747702+ansxy@users.noreply.github.com>
2022-08-10 11:12:45 +0800

    Done

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ app.post(
         res.status(500).send(err.message);
       });
   }
-); // TODO:DONE
+); // DONE
 
 app.post(
-  "/api/excercise/add",
+  "/api/users/:_id/exercises",
   bodyParser.urlencoded({ extended: true }),
   (req, res) => {
     let { userId, description, duration, date } = req.body;
@@ -58,10 +58,36 @@ app.post(
         res.status(500).send(err.message);
       });
   }
-);
+); // DONE
+
+app.get(
+  "/api/users/:_id/logs",
+  bodyParser.urlencoded({ extended: true }),
+  (req, res) => {
+    const id = req.params._id;
+    User.findById(id)
+      .then((user) => {
+        if (!user) throw new Error(`User ${id} not found`);
+        ET.find({ userId: id }).then((log) =>
+          res.status(200).send({
+            _id: req.params._id,
+            username: user.username,
+            count: log.length,
+            log: log.map((logs) => ({
+              description: logs.description,
+              duration: logs.duration,
+              date: logs.date,
+            })),
+          })
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send(err.message);
+      });
+  }
+); //DONE
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-// ET.findOne();
